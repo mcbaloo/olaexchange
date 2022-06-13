6f585d2a03222a585b652a9291745211a48fa6d6
Oluwabusayomi <Darolloladejo@gmail.com>
2022-06-13 20:14:38 +0100

    added alternative port should he not find the port to use in the env file

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ app.use((err, req, res) => {
     res.send({error: err.message});
 });
 
-app.listen(process.env.PORT,()=> console.log("started"));
+app.listen(process.env.PORT || 3000,()=> console.log("started"));
